43a996fc74a87b79d234b8181eac99f6c5266024
Eduardo Schork <eduardo.schork1814@gmail.com>
2025-07-03 01:23:09 -0300

    fix: bypass to render server

--- src/infra/http-server/express/middleware/auth.middleware.ts
+++ src/infra/http-server/express/middleware/auth.middleware.ts
@@ -4,33 +4,34 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
     // Bypass to Render server
     next();
 
-    const authHeader = req.headers.authorization;
+    // const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        res.status(401).json(false);
-        return;
-    }
+    // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    //     res.status(401).json(false);
+    //     return;
+    // }
 
-    const token = authHeader.split(" ")[1];
+    // const token = authHeader.split(" ")[1];
 
-    try {
-        const authResponse = await fetch("http://ese-authentication:3000/api/auth/check", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ token }),
-        });
+    // try {
+    //     const authResponse = await fetch("http://ese-authentication:3000/api/auth/check", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({ token }),
+    //     });
 
-        const isValid = await authResponse.json();
-        if (isValid !== true) {
-            res.status(401).json(false);
-            return;
-        }
-        next();
-    } catch (error) {
-        console.error("Erro na autenticação:", error);
-        res.status(401).json(false);
-        return;
-    }
+    //     const isValid = await authResponse.json();
+    //     if (isValid !== true) {
+    //         res.status(401).json(false);
+    //         return;
+    //     }
+
+    //     next();
+    // } catch (error) {
+    //     console.error("Erro na autenticação:", error);
+    //     res.status(401).json(false);
+    //     return;
+    // }
 }
